376055b095d4037c4c0bb7b95f04634212d0dd7a
Jeremy Helsel <JIHlucky7@gmail.com>
2023-06-14 17:33:41 -0500

    Animation polish

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -20,7 +20,7 @@ const Page = ({app}) => {
 
       <main className="bg-white bg-opacity-5 | sm:pb-4 md:h-full w-full md:w-1/2 | mx-auto md:mx-2 mb-4 md:mb-0 | px-4 pt-2 pb-2 md: pb-0 | sm:rounded-md  md:rounded | transition-all | order-first md:order-none">
         <h1 className='text-2xl md:text-4xl | flex'>{app.name}
-          <div onClick={toggleDescription} className={`h-8 w-8 | md:hidden | relative | transition-all duration-300 | ${descriptionExpanded ? '-rotate-90' : 'rotate-90'}`}><FaPlay/></div>
+          <div onClick={toggleDescription} className={`h-8 w-8 | md:hidden | relative | transition-all duration-300 delay-75 | ${descriptionExpanded ? '-rotate-90' : 'rotate-90'}`}><FaPlay/></div>
         </h1>
         <p className={`${descriptionExpanded ? '' : 'hidden'} md:block`}>{app.description}</p>
       </main>
