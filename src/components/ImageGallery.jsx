c0ec34bbadd79dbebf5e38ecf341130edb6e3f99
Jeremy Helsel <JIHlucky7@gmail.com>
2023-06-14 12:37:37 -0500

    Added functionality to dropdown chevron

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -8,9 +8,9 @@ const ImageGallery = ({app}) => {
 
     return (
         <div className="w-full h-96 md:w-1/2 md:h-3/4 | relative | group">
-            <button className="w-4 h-12 | group-hover:block | md:opacity-0 group-hover:opacity-100 | rounded | transition-all | absolute | top-40 md:top-64 left-0"><FaChevronLeft /></button>
+            {/* <button className="w-4 h-12 | group-hover:block | md:opacity-0 group-hover:opacity-100 | rounded | transition-all | absolute | top-40 md:top-64 left-0"><FaChevronLeft /></button> */}
             <img className="h-full w-full | pb-4 | rounded | object-cover object-top" src={app.image} alt="" />
-            <button className="w-4 h-12 | group-hover:block | md:opacity-0 group-hover:opacity-100 | rounded | transition-all | absolute | top-40 md:top-64 right-0"><FaChevronRight /></button>
+            {/* <button className="w-4 h-12 | group-hover:block | md:opacity-0 group-hover:opacity-100 | rounded | transition-all | absolute | top-40 md:top-64 right-0"><FaChevronRight /></button> */}
         </div>
     )
 }
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,20 +1,28 @@
+import { useState } from 'react'
+
 import LinksWidget from './LinksWidget'
 import ImageGallery from './ImageGallery'
 
 import { FaPlay } from 'react-icons/fa'
 
 const Page = ({app}) => {
+  const [descriptionExpanded, setDescriptionExpanded] = useState(false)
+
+  const toggleDescription = () => {
+    setDescriptionExpanded(!descriptionExpanded)
+  }
+
   return (
     <div className="w-full md:w-4/5 p-4 mb-4 sm:mx-auto | flex flex-col md:flex-wrap justify-start md:justify-between">
 
       <ImageGallery app={app}/>
       <LinksWidget className links={app.links}/>
 
-      <main className="bg-white bg-opacity-5 | sm:pb-4 md:h-full w-full md:w-1/2 | mx-auto md:mx-2 mb-4 md:mb-0 | px-4 pt-2 pb-2 md: pb-0 | sm:rounded-md  md:rounded | order-first md:order-none">
-        <h1 className='text-2xl md:text-4xl'>{app.name}
-          <div className='h-8 w-8 | md:hidden | relative | rotate-90'><FaPlay/></div>
+      <main className="bg-white bg-opacity-5 | sm:pb-4 md:h-full w-full md:w-1/2 | mx-auto md:mx-2 mb-4 md:mb-0 | px-4 pt-2 pb-2 md: pb-0 | sm:rounded-md  md:rounded | transition-all | order-first md:order-none">
+        <h1 className='text-2xl md:text-4xl | flex'>{app.name}
+          <div onClick={toggleDescription} className={`h-8 w-8 | md:hidden | relative | transition-all duration-300 | ${descriptionExpanded ? '-rotate-90' : 'rotate-90'}`}><FaPlay/></div>
         </h1>
-        <p>{app.description}</p>
+        <p className={`${descriptionExpanded ? '' : 'hidden'} md:block`}>{app.description}</p>
       </main>
     </div>
   )
