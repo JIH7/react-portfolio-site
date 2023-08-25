87b1619de9069bd6c3c75577f65308738d03f287
Jeremy <jihlucky7@gmail.com>
2023-08-25 13:15:45 -0500

    Added dictionary app, I need a new solution for adding apps

--- src/AppList.js
+++ src/AppList.js
@@ -3,6 +3,7 @@ import DiceToMeetYou from './assets/dicetomeetyou.jpg'
 import GameBacklogImg from './assets/GameBacklog.jpg'
 import CalculatorImg from './assets/Calculator.jpg'
 import LandingPage from './assets/ManageLanding.jpg'
+import DictionaryImage from './assets/Dictionary.jpg'
 
 class AppTile{
     constructor(name, description, image, links,  shortName) {
@@ -30,16 +31,26 @@ class LinkInfo{
 
 const AppList = [
 
-    new AppTile("Game Backlog Tracker", "This is my first full stack application with authentication. The user enters a list of video games stored to their unique account. Games can be marked as completed and stats from HowLongToBeat.com can be viewed. This app runs a NodeJS backend with the Express frameowrk. Passport is used for authentication and MongoDB with Mongoose is used for account and game lists.", GameBacklogImg, [
+    new AppTile("About me", "My name is Jeremy and I'm a Chicago based web developer with a focus on implementing front end designs and expirience building full stack web applications. I also dabble in game development, participating in the GMTK game jam every year with a few other developers.", HeadShotImg, [
+        new LinkInfo('https://github.com/JIH7/react-portfolio-site', "Source for this website"),
+        new LinkInfo('https://www.frontendmentor.io/profile/JIH7', 'My Frontend Mentor page')
+    ]),
+
+    new AppTile("Dictionary", "This is one of the apps I'm most proud of. Users can search for a word and get results from the free dictionary API, with the ability to even listen to audio recordings when available. I found it to be an amazing use case for React due to the need to render variable amounts of API data. I also found quite a few handy uses for the useEffect hooke. This project uses SCSS with a mobile first workflow and required very few media queries to finalize the tablet and desktop layouts. Great care was taken to use semantic HTML tags as well.",
+        DictionaryImage, [
+            new LinkInfo("https://helsel-dictionary-app.netlify.app/", "Live website"),
+            new LinkInfo("https://github.com/JIH7/dictionary-web-app", "Source Code")
+        ]
+    ),
+
+    new AppTile("Game Backlog Tracker", "This is my first full stack application with authentication. The user enters a list of video games stored to their unique account. Games can be marked as completed and stats from HowLongToBeat.com can be viewed. This app runs a NodeJS backend with the Express frameowrk. Passport is used for authentication and MongoDB with Mongoose is used for account and game lists.",
+        GameBacklogImg, [
         new LinkInfo('https://VideoGameBacklog.com/', 'Live website'),
         new LinkInfo('https://github.com/JIH7/game-backlog-mvc', "Source Code"),
     ], "Game Backlog"),
 
-    new AppTile("About me", "My name is Jeremy and I'm a Chicago based web developer with a focus on implementing front end designs and expirience building full stack web applications. I also dabble in game development, participating in the GMTK game jam every year with a few other developers.", HeadShotImg, [
-        new LinkInfo('https://github.com/JIH7/react-portfolio-site', "Source for this website"),
-    ]),
-
-    new AppTile("Calculator", "A calculator made with React and Tailwind. This was based on a Frontend Mentor challenge which I had completed in the past. I wasn't too happy with the original version so I decided to redo it and use it as an oppurtunity to learn React. This was also my second time using Tailwind, and I find the pair to be incredibly powerful together. I find that coupling style with the components makes for a great workflow and makes it very easy to maintain my code. I liked the combination so much I continued using it for this portfolio site.", CalculatorImg, [
+    new AppTile("Calculator", "A calculator made with React and Tailwind. This was based on a Frontend Mentor challenge which I had completed in the past. I wasn't too happy with the original version so I decided to redo it and use it as an oppurtunity to learn React. This was also my second time using Tailwind, and I find the pair to be incredibly powerful together. I find that coupling style with the components makes for a great workflow and makes it very easy to maintain my code. I liked the combination so much I continued using it for this portfolio site.",
+        CalculatorImg, [
         new LinkInfo('https://helsel-react-calculator.netlify.app/', 'Live website'),
         new LinkInfo('https://github.com/JIH7/react-tailwind-calculator', 'Source Code')
     ]),
--- src/components/LinkWidgetLI.jsx
+++ src/components/LinkWidgetLI.jsx
@@ -1,6 +1,6 @@
 const LinkWidgetLI = ({key, text}) => {
   return (
-    <a href={text.linkAdress}><li className="bg-opacity-0 hover:bg-white hover:bg-opacity-5 | rounded | transition-all" key={key}>{text.linkName}</li></a>
+    <a href={text.linkAdress}><li className="bg-opacity-0 hover:bg-white hover:bg-opacity-5 | px-2 py-1 | rounded | transition-all" key={key}>{text.linkName}</li></a>
   )
 }
 
